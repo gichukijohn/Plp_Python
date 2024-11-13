67ca2bcf54fec61655459a44bb19c051de07d706
gichukijohn <gichukijohn61@gmail.com>
2024-11-13 08:37:31 +0300

    complete

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("Enter your name? ");
+age=input("enter your age? ");
+location=input("where are you located? ");
+print("hello "+name+" you are "+age+" years old and live in "+location);
